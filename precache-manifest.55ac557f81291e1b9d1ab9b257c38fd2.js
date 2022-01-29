4e368c990a2e5ea1202e085140daa8ef915f9c5b
Jose Ignacio Alfaro <ignacioalfaro99@gmail.com>
2022-01-29 16:25:59 -0600

    Updates

---
+++ precache-manifest.55ac557f81291e1b9d1ab9b257c38fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2fef8df0fe27337e58ee3a6a9d46e5",
+    "url": "/jalfaros/journal-app/index.html"
+  },
+  {
+    "revision": "1993ee0307d40b4516ee",
+    "url": "/jalfaros/journal-app/static/css/main.74ba49e3.chunk.css"
+  },
+  {
+    "revision": "61e5037166753b7fc76c",
+    "url": "/jalfaros/journal-app/static/js/2.1c69b8c0.chunk.js"
+  },
+  {
+    "revision": "5c12ca2df883486c500ebd423c983b56",
+    "url": "/jalfaros/journal-app/static/js/2.1c69b8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1993ee0307d40b4516ee",
+    "url": "/jalfaros/journal-app/static/js/main.965f47dc.chunk.js"
+  },
+  {
+    "revision": "66d09a7cc3dfc675c917",
+    "url": "/jalfaros/journal-app/static/js/runtime-main.c679da9d.js"
+  }
+]);
